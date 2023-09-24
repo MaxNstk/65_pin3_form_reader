24f45ed4e43008567dd566b88665f5c459b13d80
Max Singular <max.starke@singular.inf.br>
2023-09-24 20:40:37 -0300

    iniciao da config

--- config.py
+++ config.py
@@ -8,21 +8,37 @@ class Config:
     template_width_px:int
 
     template_width_ss:int = 1000
-    template_heigth_ss:int = None
+    template_height_ss:int = None
 
-
-    first_cell_start_x:int
-    first_cell_end_x:int
-
-    first_cell_start_y:int
-    first_cell_start_y:int
+    first_cell_x1:int
+    first_cell_y1:int
+    first_cell_x1:int
+    first_cell_y2:int
 
     column_amount: int
+    row_amount: int
 
-    def __init__(self, template_height, template_width) -> None:
-        self.template_height = template_height
-        self.template_width = template_width
-    
+    ss_between_column: int
+    ss_between_rows: int
+
+    def __init__(self, template_width_px, template_height_px) -> None:
+        self.template_width_px = int(template_width_px)
+        self.template_height_px = int(template_height_px)
+        self.calculate_proportion()
 
     def calculate_proportion(self):
-        self.template_heigth_ss = (self.template_height_px * self.template_width_ss)/1000
+        self.template_height_ss = self.template_height_px * (self.template_width_ss / self.template_width_px)
+
+    def parse_px_posisiton_to_ss_position(self, x_px, y_px):
+        return int((x_px / self.template_width_px) * self.template_width_ss), int((y_px / self.template_height_px) * self.template_height_ss)
+
+    def to_json(self):
+        raise NotImplementedError
+    
+    def from_json(self, json_path):
+        return NotImplementedError
+    
+# c = Config(200, 100)
+
+# a, b = c.parse_px_posisiton_to_ss_position(100, 50)
+# print
