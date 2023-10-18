7cd106236b64ef0749493ed1b0cb16cc6e59f570
Max Singular <max.starke@singular.inf.br>
2023-10-17 21:02:22 -0300

    atualizando alfabeto

--- itp_forms/core/answers_interpreter.py
+++ itp_forms/core/answers_interpreter.py
@@ -10,10 +10,12 @@ from openpyxl import Workbook
 from itp_forms.core.config import Config
 from itp_forms.core.image_handler import ImageHandler
 
-
-
 XLSX_ANSWERS_FOLDER = os.path.join(settings.MEDIA_ROOT, '07_xlsx_answers_folder')
-ALPHABET = tuple('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+ALPHABET = ('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R',
+            'S','T','U','V','W','X','Y','Z','AA','AB','AC','AD','AE','AF','AG','AH','AI',
+            'AJ','AK','AL','AM','AN','AO','AP','AQ','AR','AS','AT','AU','AV','AW','AX','AY',
+            'AZ','BA','BB','BC','BD'
+            )
     
 class AnswersInterpreter:
 
