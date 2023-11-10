ab89fafaa0e417d6eeb6582d52a337379fc5b6e2
Max Singular <max.starke@singular.inf.br>
2023-11-09 21:00:03 -0300

    executáveis iniciais

--- executaveis/PrimeiraVezLinux.sh
+++ executaveis/PrimeiraVezLinux.sh
@@ -2,6 +2,6 @@
 apt-get install poppler-utils
 pip install --upgrade pip
 pip install --upgrade opencv-python
-chown -R your_username:your_group /home/max/projets/65_pin3_form_reader/
+chown -R username /home/max/projets/65_pin3_form_reader/
 source venvLinux/bin/activate
 python manage.py runserver
