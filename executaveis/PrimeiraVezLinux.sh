269d7a6083a27cc0d08366d8b31e6614c537c96b
Max Singular <max.starke@singular.inf.br>
2023-11-13 20:28:32 -0300

    modificação da estrutura de pastas

--- executaveis/PrimeiraVezLinux.sh
+++ executaveis/PrimeiraVezLinux.sh
@@ -5,4 +5,5 @@ pip install --upgrade pip
 pip install --upgrade opencv-python
 chown -R $USER ../../65_pin3_form_reader/
 source venvLinux/bin/activate
+pip install -r requirements.txt
 python manage.py runserver
