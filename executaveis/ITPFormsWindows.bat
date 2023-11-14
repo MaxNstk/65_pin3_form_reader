281f845f85a1661e86202052f3c9e5621e643bb6
Max Singular <max.starke@singular.inf.br>
2023-11-13 21:00:57 -0300

    correções nos executáveis

--- executaveis/ITPFormsWindows.bat
+++ executaveis/ITPFormsWindows.bat
@@ -1,5 +1,5 @@
 @echo off
 
-call venv\Scripts\activate
-python manage.py runserver
+call ../venv/Scripts/activate
+python ../manage.py runserver
 start http://127.0.0.1:8000
--- executaveis/PrimeiraVezWindows.bat
+++ executaveis/PrimeiraVezWindows.bat
@@ -1,6 +1,5 @@
 @echo off
-
-call venv\Scripts\activate
-call pip install -r requirements.txt
-python manage.py runserver
+call ../venv/Scripts/activate
+call pip install -r ../requirements.txt
+python ../manage.py runserver
 start http://127.0.0.1:8000
