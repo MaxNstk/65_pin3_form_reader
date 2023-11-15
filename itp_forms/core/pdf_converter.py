b69baaa6f203cec8e0627982a3ba724ac773f7f8
Max Singular <max.starke@singular.inf.br>
2023-11-15 12:21:37 -0300

    fix linux

--- itp_forms/core/pdf_converter.py
+++ itp_forms/core/pdf_converter.py
@@ -11,8 +11,6 @@ class PDFConverter:
     @staticmethod
     def convert_to_jpg(pdf_path:str, result_path:str):
         """ Converte um arquivo unico """
-        print('platform.system():  '+platform.system())
-        print("os.path.join('poppler-23.08.0','Library','bin') " +os.path.join(settings.BASE_DIR,'poppler-23.08.0','Library','bin'))
 
         if platform.system() == 'Windows':
             images = convert_from_path(pdf_path, poppler_path=os.path.join(settings.BASE_DIR, 'poppler-23.08.0','Library','bin'))
@@ -26,6 +24,9 @@ class PDFConverter:
     def convert_to_pdf_massive(pdf_path:str, folder:str):
 
         """ Converte um conjunto de arquivos, recebe a pasta e coloca o indice dele em frente a cada um """
-        images = convert_from_path(pdf_path, poppler_path=os.path.join(settings.BASE_DIR, 'poppler-23.08.0','Library','bin'))
+        if platform.system() == 'Windows':
+            images = convert_from_path(pdf_path, poppler_path=os.path.join(settings.BASE_DIR, 'poppler-23.08.0','Library','bin'))
+        else:
+            images = convert_from_path(pdf_path)
         for idx, image in enumerate(images, 1):
             image.save(os.path.join(folder,f'p{idx}.jpeg'), 'JPEG')
