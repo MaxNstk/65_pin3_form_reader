ba0236ae6b4db9047ed89b0b4a3db65796806332
Max Singular <max.starke@singular.inf.br>
2023-11-16 18:55:31 -0300

    executáveis

--- executaveis/PrimeiraVezLinux.sh
+++ executaveis/PrimeiraVezLinux.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+sudo apt update && sudo apt upgrade -y
+sudo apt install software-properties-common -y
+sudo apt install python3.10
 apt-get install poppler-utils
-pip install --upgrade pip
-pip install --upgrade opencv-python
+python3.10 -m venv ../venvLinux
 chown -R $USER ../../65_pin3_form_reader/
 source ../venvLinux/bin/activate
+pip install --upgrade pip
+pip install --upgrade opencv-python
 pip install -r ../requirements.txt
 python ../manage.py runserver
